1beab38da7e59d4020fb8c693ac9475aa49c0b67
Devanand-PA <devanandpa721@gmail.com>
2023-10-06 00:47:39 +0530

    only unique entries are shown now

--- scholar_search/sch.sh
+++ scholar_search/sch.sh
@@ -9,4 +9,4 @@ do
 curl -A 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0' "https://scholar.google.com/scholar?hl=en&start=${i}0&as_sdt=0%252C5&q=$searchquery" > /tmp/sch_sh/sci_sh_test_${searchquery}${unix_millis}${i}.html
 sed -i 's/>/>\n/g' /tmp/sch_sh/sci_sh_test_${searchquery}${unix_millis}${i}.html
 done
-firefox $(cat /tmp/sch_sh/sci_sh_test_${searchquery}${unix_millis}*.html  | grep href | grep -v accounts.google | grep -e pdf -e html| awk -F "\"" '{print $2}')
+firefox $(cat /tmp/sch_sh/sci_sh_test_${searchquery}${unix_millis}*.html  | grep href | grep -v accounts.google | grep -e pdf -e html| awk -F "\"" '{print $2}' | uniq)
